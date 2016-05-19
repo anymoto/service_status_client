576ad697a1ece5a061b6054f44c9aa9b8d8bf8d0
Ana Castro <ana@castromartinez.com>
2016-05-18 20:11:54 -0500

    WIP activeresource integration

--- lib/service_status_client.rb
+++ lib/service_status_client.rb
@@ -1,19 +1,37 @@
 require "service_status_client/version"
-require 'active_resource'
 
 module ServiceStatusClient
   class << self
-    attr_accessor :token, :url
+    attr_accessor :auth_token, :url
 
-    def authenticate(url, token)
+    def authenticate(url, auth_token)
       @url = url
-      @token = token
+      @auth_token = auth_token
+
+      self::Base.site = url
+      self.token = auth_token
+    end
+
+    def token=(value)
+      resources.each do |klass|
+        klass.headers['Authorization'] = "Token token=#{ value }"
+      end
+    end
+
+    def resources
+      @resources ||= []
     end
   end
 
   class Base < ActiveResource::Base
+    self.format = :json
+
+    def self.inherited(base)
+      ServiceStatusClient.resources << base
+      super
+    end
   end
 
-  class Status < Base
+  class StatusMessage < Base
   end
 end
